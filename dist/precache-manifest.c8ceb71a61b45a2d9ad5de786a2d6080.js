28f9c515f85ce96a11d644022ee9ce7acb108e4f
5tsv <124922326+5tsv@users.noreply.github.com>
2024-01-28 22:25:02 +0800

    Add files via upload

---
+++ dist/precache-manifest.c8ceb71a61b45a2d9ad5de786a2d6080.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0da2de1c474745929ca",
+    "url": "css/about.1d64e71d.css"
+  },
+  {
+    "revision": "760fcc75bfb35442e776",
+    "url": "css/app.e4c919b7.css"
+  },
+  {
+    "revision": "e65f7971c7b0c11a77a8",
+    "url": "css/chunk-vendors.8a465a1d.css"
+  },
+  {
+    "revision": "ca369f574ca8bfff0ab1",
+    "url": "css/detail.f0c88d0c.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "f9a3fb0e145017e166dd4d91d9280cc4",
+    "url": "fonts/iconfont.f9a3fb0e.woff"
+  },
+  {
+    "revision": "f39ecc1a1d2a1eff3aca8aadd818bb61",
+    "url": "fonts/popfont.f39ecc1a.ttf"
+  },
+  {
+    "revision": "6c094b6d4ae9404dbed273c41b06fae8",
+    "url": "fonts/shelffont.6c094b6d.ttf"
+  },
+  {
+    "revision": "42d1462aa2de41b9b656b375d6cc9775",
+    "url": "index.html"
+  },
+  {
+    "revision": "e0da2de1c474745929ca",
+    "url": "js/about.3f0f5b5d.js"
+  },
+  {
+    "revision": "eb19fcb0c76b56c504cb",
+    "url": "js/about~detail.e61cbfb6.js"
+  },
+  {
+    "revision": "760fcc75bfb35442e776",
+    "url": "js/app.3f6df7fb.js"
+  },
+  {
+    "revision": "e65f7971c7b0c11a77a8",
+    "url": "js/chunk-vendors.3b8fd9a0.js"
+  },
+  {
+    "revision": "ca369f574ca8bfff0ab1",
+    "url": "js/detail.9b656584.js"
+  },
+  {
+    "revision": "b46d04eb43bc31ca0f9f95121646440d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
